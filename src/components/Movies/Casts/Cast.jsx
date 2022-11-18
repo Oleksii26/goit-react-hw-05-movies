689ac8ce2127542497c1b074ef5af039b5f452b6
Oleksii26 <Kolesnik26@ukr.net>
2022-11-18 20:12:16 +0200

    save

--- src/components/Movies/Casts/Cast.jsx
+++ src/components/Movies/Casts/Cast.jsx
@@ -1,14 +1,8 @@
 
 export const Cast = () => {
     return <ul>
-        <li> <img width='110' height='180' />
-            <p>Silvestr Stalone</p>
-        </li>
-        <li> <img width='110' height='180' />
-            <p>Arnold Swarts</p>
-        </li>
-        <li> <img width='110' height='180' />
-            <p>Jim Carry</p>
-        </li>
+        <li> <p>Silvestr Stalone</p>    </li>
+        <li>  <p>Arnold Swarts</p>   </li>
+        <li><p>Jim Carry</p> </li>
     </ul>
 }
